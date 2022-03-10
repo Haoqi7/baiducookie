bff310d0c57142ba06c8abd7a7c4b34e9cac0c87
Haoqi7 <w00989988@gmail.com>
2022-03-11 01:17:43 +0800

    baiducookie

---
+++ introduction.md
@@ -0,0 +1,30 @@
+# 获取百度网盘 BDUSS值 STOKEN值 完整COOKIES值
+
+```
+https://github.com/dylanbai8/GetBaiduPanCookies/releases/download/6.0/GetBaiduPanCookies.zip
+```
+
+# 扩展插件界面截图
+
+![image](https://user-images.githubusercontent.com/26950227/128627434-e80a848f-a83b-4e32-9b12-131ea3c040b6.png)
+
+![image](https://user-images.githubusercontent.com/26950227/128627508-e28813a8-3f6a-4d02-8b75-8bf28d8a5ce4.png)
+
+# 1. 下载并解压到文件夹
+
+![image](https://user-images.githubusercontent.com/26950227/128114036-f222386f-7328-4fc8-ad38-8581f1fb9968.png)
+
+# 2.安装到chrome扩展程序
+
+![image](https://user-images.githubusercontent.com/26950227/128114323-b814add4-efc7-4ae0-a999-03899bf0ff70.png)
+
+![image](https://user-images.githubusercontent.com/26950227/128114415-0614ea61-2fe9-4f4c-b164-132333806f44.png)
+
+![image](https://user-images.githubusercontent.com/26950227/128114511-8a0a1a7c-1b9e-4938-818b-f0183ba70d2b.png)
+
+![image](https://user-images.githubusercontent.com/26950227/128114672-21d3212c-84aa-49ba-8824-c9a58c263cbc.png)
+
+# 3.edge等chrome内核浏览器同样适用
+
+![image](https://user-images.githubusercontent.com/26950227/128114950-23bd4c9c-9f19-429f-9ebe-5eae4fbe0a30.png)
+
---
+++ main.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', function() {
+  chrome.tabs.getSelected(function(tab) {
+
+    chrome.cookies.getAll(
+      {
+        url: 'https://pan.baidu.com',
+        name: 'BDUSS'
+      },
+      function(cookies) {
+        var BAIDU_BDUSS = "";
+        for (var i in cookies) {
+        var BAIDU_BDUSS = cookies[0].value;
+        }
+        if (!BAIDU_BDUSS) {BAIDU_BDUSS = "您未登录百度网盘";}
+        document.getElementById("cookies-BDUSS").innerText = BAIDU_BDUSS;
+      }
+    );
+
+    chrome.cookies.getAll(
+      {
+        url: 'https://pan.baidu.com',
+        name: 'STOKEN'
+      },
+      function(cookies) {
+        var BAIDU_STOKEN = "";
+        for (var i in cookies) {
+        var BAIDU_STOKEN = cookies[0].value;
+        }
+        if (!BAIDU_STOKEN) {BAIDU_STOKEN = "您未登录百度网盘";}
+        document.getElementById("cookies-STOKEN").innerText = BAIDU_STOKEN;
+      }
+    );
+
+    chrome.cookies.getAll(
+      {
+        url: 'https://pan.baidu.com'
+      },
+      function(cookies) {
+        var BAIDU_ALL = "";
+        for (var i in cookies) {
+          BAIDU_ALL += cookies[i].name + "=" + cookies[i].value + ";";
+        }
+        if (!BAIDU_ALL) {BAIDU_ALL = "您未登录百度网盘";}
+        document.getElementById("cookies-ALL").innerText = BAIDU_ALL;
+      }
+    );
+
+    chrome.cookies.getAll(
+      {
+        url: 'https://pan.baidu.com'
+      },
+      function(cookies) {
+        var BAIDU_JSON = "";
+        var BAIDU_JSON = JSON.stringify(cookies);
+        if (!BAIDU_JSON || BAIDU_JSON === "[]") {BAIDU_JSON = "您未登录百度网盘";}
+        document.getElementById("cookies-JSON").innerText = BAIDU_JSON;
+      }
+    );
+
+  })
+})
